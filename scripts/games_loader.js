31d76cb6f4eb6f2957a1fedcc08258330372a858
AyyDev <ayydev22@gmail.com>
2026-05-30 16:39:08 +0700

    Update games_loader.js

--- scripts/games_loader.js
+++ scripts/games_loader.js
@@ -1,9 +1,7 @@
-import { data } from "./config.js"
-
-export function loadGames() {
+export function getGames() {
   const response = await fetch(
     `https://ayydev-games-api.akunffbya.workers.dev?time=${Date.now()}`
   );
   
-  data.games = await response.json();
+  return await response.json()
 }
