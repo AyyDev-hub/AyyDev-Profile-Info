ce0496222bfb581e7387d7a4f8e4c4ae7471936a
AyyDev <ayydev22@gmail.com>
2026-05-30 13:15:38 +0700

    Add files via upload

---
+++ scripts/game_list.js
@@ -0,0 +1,58 @@
+function goBack() {
+  window.location.href = "../index.html";
+}
+
+async function loadGames() {
+  const games = document.getElementById("games");
+
+  try {
+    const response = await fetch(
+      `https://ayydev-games-api.akunffbya.workers.dev?time=${Date.now()}`
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    // Validasi array
+    if (!Array.isArray(data)) {
+      games.innerHTML = "<p>Data game invalid.</p>";
+      return;
+    }
+
+    games.innerHTML = "";
+
+    data.forEach(game => {
+      games.innerHTML += `
+        <div class="card">
+          <img
+            src="${game.image}"
+            alt="${game.title}"
+          >
+
+          <div class="content">
+            <h2>${game.title}</h2>
+
+            <p>${game.description}</p>
+
+            <a
+              class="btn"
+              href="${game.link}"
+              target="_blank"
+            >
+              Play Game
+            </a>
+          </div>
+        </div>
+      `;
+    });
+
+  } catch (err) {
+    console.error(err);
+
+    games.innerHTML =
+      "<p>Gagal mengambil game.</p>";
+  }
+}
+
+loadGames();
